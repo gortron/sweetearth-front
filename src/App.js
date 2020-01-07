2dce2d1bbea15f67b1d02716a81f07a90d99b2c8
gortron <gordon.lanza@gmail.com>
2020-01-07 15:50:13 +0000

    made a Checkout component for payment through Stripe

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-
+import { Elements, StripeProvider } from "react-stripe-elements";
+import CheckoutForm from "./components/CheckoutForm";
 import { Router, Route, Switch } from "react-router-dom";
 import history from "./utils/history";
 
 function App() {
   return (
-    <div className="App">
-      <Router history={history}>
-        <header>
-          <NavBar />
-        </header>
-        <Switch>
-          <Route path="/" exact />
-        </Switch>
-      </Router>
-    </div>
+    <StripeProvider apiKey="pk_test_waOqfE4v56zJkQEG6l4EgKUD004Ku9v3wY">
+      <div className="App">
+        <Router history={history}>
+          <header>
+            <NavBar />
+            {/* <Elements>
+              <CheckoutForm />
+            </Elements> */}
+          </header>
+          <Switch>
+            <Route path="/" exact />
+          </Switch>
+        </Router>
+      </div>
+    </StripeProvider>
   );
 }
 
