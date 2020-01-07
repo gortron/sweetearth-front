5e1e1435a997ae88c2bf3f318c12ece1d1de7e95
gortron <gordon.lanza@gmail.com>
2020-01-07 13:52:32 +0000

    set up Auth0 authentication flow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
+
+import { Router, Route, Switch } from "react-router-dom";
+import history from "./utils/history";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
     </div>
   );
 }
