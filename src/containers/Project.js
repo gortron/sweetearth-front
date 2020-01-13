de9375b6ce1a0634c7a6d79b8a54cb9307dbfd0c
gortron <gordon.lanza@gmail.com>
2020-01-13 11:33:32 +0000

    Project.js now successfully filters all projects for the project the user wants to view, and will fetch necessary data if it has not been passed in as props

--- src/containers/Project.js
+++ src/containers/Project.js
@@ -4,40 +4,38 @@ import ProjectCard from "../components/ProjectCard";
 
 const Project = props => {
   const { data, getProjects } = props;
-  // const project = data.filter(project => project);
-  // const [project, setProject] = useState(null);
+
+  const [project, setProject] = useState(null);
 
   useEffect(() => {
-    // !data ? getProjects() : null;
     if (!data) getProjects();
-    // let proj = data.filter(proj => )
+    if (data && !project) findProject();
   });
 
   const findProject = () => {
     // Write a function that filters this project from all of the projects. It will need to look at the URL, figure out its name, then filter for name.
     let projectPath = window.location.pathname.split("/").slice(-1)[0];
-    let projectName = projectPath.split("-");
+    let projectName = projectPath.split("-").join(" ");
+    let capitalizedProjectName = projectName.replace(/\b\w/g, l =>
+      l.toUpperCase()
+    );
+    let project = data.filter(
+      project => project.attributes.name === capitalizedProjectName
+    )[0];
+    setProject({ ...project.attributes });
   };
 
-  // const getProject = async () => {
-  //   const endpoint = "http://localhost:3000" + window.location.pathname;
-  //   const response = await fetch(endpoint);
-  //   const data = await response.json();
-  //   const proj = data.data.attributes;
-  //   setProject(proj);
-  // };
-
   const handleRender = () => {
-    return !data ? (
+    return !data || !project ? (
       <p>Loading...</p>
     ) : (
       <Container className="content">
-        {/* <Header
+        <Header
           inverted
           as="h1"
           content={project.name}
           style={{ fontSize: "3em" }}
-        ></Header> */}
+        ></Header>
       </Container>
     );
   };
