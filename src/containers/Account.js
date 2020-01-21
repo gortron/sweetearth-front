5b668858653edacd29a1122cab1d10ce68d24fbc
gortron <gordon.lanza@gmail.com>
2020-01-21 14:00:14 +0000

    improved Account view

--- src/containers/Account.js
+++ src/containers/Account.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { useAuth0 } from "../utils/react-auth0";
-import { Container, Header, Button } from "semantic-ui-react";
+import { Container, Header, Button, Image, Divider } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const Account = () => {
   const { loading, user, logout } = useAuth0();
@@ -8,7 +9,7 @@ const Account = () => {
 
   useEffect(() => {
     if (user && !userData) getUser();
-  });
+  }, [user]);
 
   const getUser = async () => {
     // this function should take the authenticated user's email address, and use it to query the backend for the user's pledge history
@@ -18,15 +19,42 @@ const Account = () => {
     setUserData(data);
   };
 
+  const formatDate = pledge => {
+    let yyyymmdd = pledge.created_at.split("T")[0].split("-");
+    let [year, month, day] = yyyymmdd;
+    let mmdd = `${month}/${day}`;
+    return mmdd;
+  };
+
+  const projectUrl = name => {
+    return "/projects/" + name.replace(/\s+/g, "-").toLowerCase();
+  };
+
   const renderPledges = () => {
     if (userData) {
       return userData.pledges.map((pledge, idx) => {
         return (
-          <div key={idx} className="user-pledges">
-            <Header>
-              ${pledge.amount / 100} for {pledge.project.name}
-            </Header>
-          </div>
+          <Fragment>
+            <div key={idx} className="user-pledges">
+              <Image src={pledge.project.imgUrl} style={{ width: "30%" }} />
+              <div>
+                <Header style={{ color: "#758E50", fontSize: "2em" }}>
+                  ${pledge.amount / 100}
+                </Header>
+                <Header style={{ fontSize: "1.33em" }}>
+                  on {formatDate(pledge)} for {pledge.project.name}
+                </Header>
+              </div>
+              <Button
+                as={Link}
+                to={projectUrl(pledge.project.name)}
+                style={{ alignSelf: "center" }}
+              >
+                View Project
+              </Button>
+            </div>
+            <Divider />
+          </Fragment>
         );
       });
     }
@@ -45,8 +73,7 @@ const Account = () => {
         This is your account page. Here, you can see a history of your pledges,
         or log out.
       </p>
-      <br />
-      <br />
+      <Divider style={{ marginTop: "10px", marginBottom: "10px" }} />
       <Container className="account">
         <Header
           as="h1"
